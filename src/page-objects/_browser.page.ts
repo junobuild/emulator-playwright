8a68db1aa0bd7b20516bdebcd215a6e8d7152f5d
David Dal Busco <david.dalbusco@outlook.com>
2026-02-20 09:16:04 +0100

    feat: make cli close revert config optional

--- src/page-objects/_browser.page.ts
+++ src/page-objects/_browser.page.ts
@@ -1,5 +1,4 @@
 import type {Browser, BrowserContext, Page} from '@playwright/test';
-import {TestPage} from './_page';
 
 export interface BrowserPageParams {
   page: Page;
@@ -7,22 +6,17 @@ export interface BrowserPageParams {
   browser: Browser;
 }
 
-export abstract class BrowserPage extends TestPage {
+export abstract class BrowserPage {
   protected readonly page: Page;
   protected readonly context: BrowserContext;
   protected readonly browser: Browser;
 
   protected constructor({page, context, browser}: BrowserPageParams) {
-    super();
-
     this.page = page;
     this.context = context;
     this.browser = browser;
   }
 
-  /**
-   * @override
-   */
   async close(): Promise<void> {
     await this.page.close();
   }
